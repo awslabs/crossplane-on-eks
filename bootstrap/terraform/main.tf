d8b7befbb099737889b6a03e005da2e72f7c0b24
Carlos Santana <csantana23@gmail.com>
2022-12-01 15:07:58 -0500

    add cluster id
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- bootstrap/terraform/main.tf
+++ bootstrap/terraform/main.tf
@@ -86,7 +86,7 @@ module "eks_blueprints" {
   #source         = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons"
   source = "github.com/csantanapr/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=crossplane-updates-11-28"
 
-  #eks_cluster_id       = module.eks_blueprints.eks_cluster_id
+  eks_cluster_id       = module.eks_blueprints.eks_cluster_id
   #eks_cluster_endpoint = module.eks_blueprints.eks_cluster_endpoint
   #eks_oidc_provider    = module.eks_blueprints.oidc_provider
   #eks_cluster_version  = module.eks_blueprints.eks_cluster_version
