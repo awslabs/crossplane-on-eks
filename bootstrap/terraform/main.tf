359e240778d098879209b0b9d6c630ab4cced1e8
Carlos Santana <csantana23@gmail.com>
2023-01-26 12:22:21 -0500

    set default node size to m5.large
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- bootstrap/terraform/main.tf
+++ bootstrap/terraform/main.tf
@@ -103,7 +103,7 @@ module "eks_blueprints" {
   managed_node_groups = {
     mg = {
       node_group_name = "managed-on-demand"
-      instance_types  = ["t3.small"]
+      instance_types  = ["m5.large"]
       min_size        = 2
       subnet_ids      = module.vpc.private_subnets
     }
