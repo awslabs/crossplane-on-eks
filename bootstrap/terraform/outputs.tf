4c88dfa9da13807c98c3bdbf22ed71d1854c9271
Christina Andonov <candonov@amazon.com>
2023-03-27 18:29:11 -0700

    upgrades

--- bootstrap/terraform/outputs.tf
+++ bootstrap/terraform/outputs.tf
@@ -1,8 +1,8 @@
 output "eks_cluster_id" {
   description = "Kubernetes Cluster Name"
-  value       = module.eks_blueprints.eks_cluster_id
+  value       = module.eks.cluster_id
 }
 output "configure_kubectl" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = module.eks_blueprints.configure_kubectl
+  value       = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --alias ${var.name} --region ${var.region}"
 }
