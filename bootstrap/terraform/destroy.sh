768bf0eb001be9437342aaec19d5a62d8d474066
candonov <25967713+candonov@users.noreply.github.com>
2023-06-06 12:01:10 -0700

    updating versions and configs (#129)

--- bootstrap/terraform/destroy.sh
+++ bootstrap/terraform/destroy.sh
@@ -2,7 +2,8 @@
 
 set -xe
 
-terraform destroy -target="module.eks_blueprints_kubernetes_addons" -auto-approve
+terraform destroy -target="module.eks_blueprints_addons" -auto-approve
+terraform destroy -target="module.eks_blueprints_crossplane_addons" -auto-approve
 terraform destroy -target="module.eks" -auto-approve
 terraform destroy -target="module.vpc" -auto-approve
 terraform destroy -auto-approve
