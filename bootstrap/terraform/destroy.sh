27bed60a1375a81641721a9f9133888e4d1750ef
Christina Andonov <candonov@amazon.com>
2023-09-09 21:48:34 -0700

    updating destroy script

--- bootstrap/terraform/destroy.sh
+++ bootstrap/terraform/destroy.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
+terraform destroy -target="module.crossplane" -auto-approve
 terraform destroy -target="module.eks_blueprints_addons" -auto-approve
-terraform destroy -target="module.eks_blueprints_crossplane_addons" -auto-approve
 terraform destroy -target="module.eks" -auto-approve
 terraform destroy -target="module.vpc" -auto-approve
 terraform destroy -auto-approve
